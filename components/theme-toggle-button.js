219e8943e97da9681c1bc368888d7de6874d30fa
Takuya Matsuyama <hi@craftz.dog>
2021-10-08 20:24:48 +0900

    feat(theme): animate toggle button

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -1,12 +1,27 @@
-import { Button, useColorMode } from "@chakra-ui/react";
+import { AnimatePresence, motion } from "framer-motion";
+import { IconButton, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 
 const ThemeToggleButton = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Button onClick={toggleColorMode} variant="outline">
-      Toggle {colorMode === "light" ? "Dark" : "Light"}
-    </Button>
+    <AnimatePresence exitBeforeEnter initial={false}>
+      <motion.div
+        key={useColorModeValue("light", "dark")}
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 20, opacity: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        <IconButton
+          aria-label="Toggle theme"
+          colorScheme={useColorModeValue("purple", "orange")}
+          icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+          onClick={toggleColorMode}
+        ></IconButton>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
